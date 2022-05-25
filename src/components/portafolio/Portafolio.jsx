97b50239ac1c2ac75ef013a35c819bb402b6ac18
besta-systems <100731792+besta-systems@users.noreply.github.com>
2022-05-25 11:12:21 -0500

    ajuste de iconos whatsapp y telefono

--- src/components/portafolio/Portafolio.jsx
+++ src/components/portafolio/Portafolio.jsx
@@ -8,21 +8,10 @@ function Portafolio() {
             <h1>Beneficios</h1>
             <h3>Un producto integral</h3>
             <div className="container">
-                <div className="item">
-                    <img src="./assets/taller.png" alt=""/>
-                    <h3>BESTA</h3>
-                </div>
-                <div className="item">
-                    <img src="./assets/taller2.png" alt=""/>
-                    <h3>BESTA</h3>
-                </div>
-                <div className="item">
-                    <img src="./assets/gps2.png" alt=""/>
-                    <h3>BESTA</h3>
-                </div>
-                <div className="item">
-                    <img src="./assets/carro2.png" alt=""/>
-                    <h3>BESTA</h3>
+                <div className="vid">
+                <video loop controls>
+                    <source src="https://video.xx.fbcdn.net/v/t66.36240-2/10000000_314416734201139_3129767750125027824_n.mp4?_nc_cat=109&ccb=1-7&_nc_sid=985c63&efg=eyJybHIiOjE3NTgsInJsYSI6MjQ4NiwidmVuY29kZV90YWciOiJvZXBfaGQifQ%3D%3D&_nc_ohc=cKpMTttzxdcAX9mjnqz&rl=1758&vabr=1172&_nc_ht=video-iad3-1.xx&oh=00_AT_wLaZDXaqL7YCV0_OF3gIfryFSSQEEPZ9oeveYsDAS-Q&oe=62928828" type="video/mp4" />
+                    </video>
                 </div>
             </div>
         </div>
