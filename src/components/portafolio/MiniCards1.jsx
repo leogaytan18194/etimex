2075c96e1f5e1de84492f69d02a03a6ec3206890
besta-systems <100731792+besta-systems@users.noreply.github.com>
2022-06-08 15:27:54 -0500

    eliminar shadow en beneficios

--- src/components/portafolio/MiniCards1.jsx
+++ src/components/portafolio/MiniCards1.jsx
@@ -8,10 +8,8 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  -webkit-box-shadow: 0px 0px 17px -11px rgba(0, 0, 0, 0.58);
-  box-shadow: 0px 0px 17px -11px rgba(0, 0, 0, 0.58);
   @media only screen and (max-width: 480px) {
-    width: 50px
+    width: 100px
   }
 `;
 
--- src/components/requisitos/Requisitos.jsx
+++ src/components/requisitos/Requisitos.jsx
@@ -1,70 +1,74 @@
 import React from 'react'
 import './requisitos.scss';
-import {
-    Box,
-    Card,
-    CardContent,
-    Typography,
-  } from "@material-ui/core";
-  import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
-
-  const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: "flex",
-      flexWrap: "wrap",
-      "& > *": {
-        margin: theme.spacing(3),
-      },
-    },
-  })
-);
 
 function Requisitos() {
-    const classes = useStyles();
+
+
   return (
     <div className="requisitos" id="requisitos">
     <h1>¿Que necesitas para obtener un auto con BESTA?</h1>
-    <Box className={classes.root}>
-      <Card>
-        <CardContent>
-          <Typography variant="subtitle1">Tener una licencia vigente tipo C</Typography> <img src="assets/licencia.png" alt="" />
-        </CardContent>
-        
-      </Card>
-      <Card>
-        <CardContent>
-          <Typography variant="subtitle1">Captura de tus viajes en plataformas como </Typography> <img src="assets/plataformas-conductor.png" alt="" /> 
-        </CardContent>
-        
-      </Card>
-      <Card>
-        <CardContent>
-          <Typography variant="subtitle1">Identificación oficial vigente INE/IFE</Typography><img src="assets/identi.png" alt="" />
-        </CardContent>
-        
-      </Card>
-    </Box>
-    <Box className={classes.root}>
-      <Card>
-        <CardContent>
-          <Typography variant="subtitle1">Contar con comprobante de domicilio</Typography> <img src="assets/licencia.png" alt="" />
-        </CardContent>
-        
-      </Card>
-      <Card>
-        <CardContent>
-          <Typography variant="subtitle1">Captura de tus viajes en plataformas como </Typography> <img src="assets/plataformas-conductor.png" alt="" /> 
-        </CardContent>
-        
-      </Card>
-      <Card>
-        <CardContent>
-          <Typography variant="subtitle1">Identificación oficial vigente INE/IFE</Typography><img src="assets/identi.png" alt="" />
-        </CardContent>
-        
-      </Card>
-    </Box>
+    <div className="container">
+          <div className="card featured">
+            <div className="top">
+              <img
+                className="user"
+                src="assets/safety.png"
+                alt=""
+              />
+            </div>
+            <div className="bottom">
+              <h3>Seguro</h3>
+            </div>
+          </div>
+          <div className="card featured">
+            <div className="top">
+              <img
+                className="user"
+                src="assets/identi.png"
+                alt=""
+              />
+            </div>
+            <div className="bottom">
+              <h3>Identificación oficial vigente</h3>
+            </div>
+          </div>
+          <div className="card featured">
+            <div className="top">
+              <img
+                className="user"
+                src="assets/licencia.png"
+                alt=""
+              />
+            </div>
+            <div className="bottom">
+              <h3>Licencia de conducir vigente</h3>
+            </div>
+          </div>
+          <div className="card featured">
+            <div className="top">
+              <img
+                className="user"
+                src="assets/plataformas-conductor.png"
+                alt=""
+              />
+            </div>
+            <div className="bottom">
+              <h3>Viajes en plataformas de conductor privado</h3>
+            </div>
+          </div>
+          <div className="card featured">
+            <div className="top">
+              <img
+                className="user"
+                src="assets/refrendo.png"
+                alt=""
+              />
+            </div>
+            <div className="bottom">
+              <h3>Comprobante de domicilio</h3>
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
