132232ecb62668c7705333a01689a1683ed4582d
besta-systems <100731792+leogaytan@users.noreply.github.com>
2022-09-21 17:30:01 -0500

    formulario listo

--- src/components/formulario/Formulario.jsx
+++ src/components/formulario/Formulario.jsx
@@ -4,10 +4,13 @@ import {  useRef , useState, useEffect } from "react";
 import emailjs from '@emailjs/browser';
 import { useParams } from 'react-router-dom'
 import axios from "axios";
+import calendar from "./img/yearCalendar.png";
+import carrito from "./img/estandaricono.png";
+import galardon from "./img/nuevoicono.png";
+import tiempo from "./img/icotiempo.png";
 
 
 const Formulario = (props) => {
-
 
 
     const { id } = useParams()
@@ -25,7 +28,7 @@ const Formulario = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_fst7gdm', 'template_oblxilh', form.current, 'QQvoOXLmHkR29h36W')
+    emailjs.sendForm('service_fst7gdm', 'template_3il5sqt', form.current, 'QQvoOXLmHkR29h36W')
     .then(
       (result) => {
         console.log(result.text);
@@ -45,13 +48,13 @@ const Formulario = (props) => {
         <h1 className="o-title"><strong>{carro.modelo}</strong></h1>
         <h2 className="o-title2">{carro.marca}</h2>
         <div className="detalles-form">
-            <img src={carro.carrito} alt="" className="imgMiniAuto"/> <p className="texto-form">&nbsp; Estándar &nbsp;</p> 
-            <img src={carro.calendario} alt="" className="imgMini"/> <p className="texto-form"> &nbsp; 2022 &nbsp;</p>
-            <img src={carro.galardon} alt="" className="imgMini"/> <p className="texto-form"> &nbsp;Nuevo </p>
+            <img src={carrito} alt="" className="imgMini2"/> <p className="texto-form">&nbsp; Estándar &nbsp;</p> 
+            <img src={calendar} alt="" className="imgMini"/> <p className="texto-form"> &nbsp; 2022 &nbsp;</p>
+            <img src={galardon} alt="" className="imgMini"/> <p className="texto-form"> &nbsp;Nuevo </p>
         </div>
           <div className="c-info-item-form">
             <img src={carro.imagen} alt="" className="c-imagen" align="left"/>
-            <img src={carro.calendario} alt="" className="imgMini"/><p className="texto-meses"> &nbsp; <strong> 36 Meses </strong></p>
+            <img src={tiempo} alt="" className="imgMini"/><p className="texto-meses"> &nbsp; <strong> 36 Meses </strong></p>
             <br/>
             <p>*Con opción de compra</p>
             <br />
@@ -61,24 +64,44 @@ const Formulario = (props) => {
             <br />
             <p className="montos"><strong> $17,000 / </strong></p>
             <p className="montos">Depósito en Garantía</p>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <p>Consulta términos y condiciones</p>
           </div>
         </div>
         <div className="c-right">
           <div className="formulario">
-          <p className="c-desc">
-            <h3>¡Déjanos tus datos!</h3> 
-            <p>Nosotros te contactamos</p>
-          </p>
+          <div className="c-desc-form">
+            <h3>Se parte de BESTA,</h3> 
+            <h2>¡Bienvenido!</h2>
+          </div>
           <form ref={form} onSubmit={handleSubmit}>
             <input  type="text" placeholder="Nombre Completo" name="nombre" required/>
-            <input  type="text" placeholder="Telefono" name="telefono" required/>
+            <input  type="text" placeholder="Celular con whatsapp" name="telefono" required/>
+            <input  type="hidden" placeholder="interes" name="auto" value={carro.modelo} required/>
             <label htmlFor="">Selecciona una opción: </label> 
             <select name="apps" required>
             <option value="uber">Uber</option>
             <option value="didi">Didi</option>
             <option value="uber y didi">Uber y Didi</option>
             <option value="otros">Otros</option>
-          </select>
+            </select><br /><br />
+            <label htmlFor="">¿Cuántos viajes llevas realizados?: </label> 
+            <select name="viajes" required>
+            <option value="De 100 a 500">De 100 a 500</option>
+            <option value="De 501 a 1,000"> De 501 a 1,000</option>
+            <option value="Más de 1,000">Más de 1,000</option>
+            </select><br /><br />
+            <label htmlFor="">¿En que parte de México vives?: </label> 
+            <select name="estado" required>
+            <option value="Nuevo León">Nuevo León</option>
+            <option value="Guadalajara"> Guadalajara</option>
+            <option value="Otro">Otro</option>
+            </select><br /><br />
             <textarea  rows="5" placeholder="Mensaje" name="mensaje" />
             <button>Enviar</button>
             {done && "Gracias solicitud enviada..."}
