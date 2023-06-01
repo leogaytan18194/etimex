3990e9f4a08fdfdd837d478fe3cb63fd0f714132
EdgarRamosLuna <eramos@besta.mx>
2023-06-01 15:22:08 -0600

    Pasos para ser parte de besta
    Modificar el apartado de "Únete a nuestra comunidad BESTA"

--- src/components/requisitos/Requisitos.jsx
+++ src/components/requisitos/Requisitos.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import './requisitos.scss';
+import styled from 'styled-components'
+import { ArrowNext } from '@styled-icons/fluentui-system-regular/ArrowNext'
 
 function Requisitos() {
-
+    const ArrowNextS = styled(ArrowNext)`scale: 0.5;`
     return (
         <div className="requisitos" id="comunidad">
             <h1>Ser parte de la comunidad BESTA es fácil, sencillo y rápido.</h1>
@@ -25,6 +27,11 @@ function Requisitos() {
                         <p className="textoRequisitos">Y comienza tu proceso de renta con opción a compra.</p>
                     </div>
                 </div>
+                <div className='arrow'>
+                    <div className='arrow-container'>
+                        <ArrowNextS />
+                    </div>
+                </div>
                 <div className="card featured">
                     <div className="top">
                         <div className='top-img-container'>
@@ -32,7 +39,6 @@ function Requisitos() {
                                 className="requisitosImagen"
                                 src="/assets/pasos/paso2.png"
                                 alt=""
-
                             />
                         </div>
                         <div className='top-txt-container'>
@@ -41,8 +47,12 @@ function Requisitos() {
 
                     </div>
                     <div className="bottom">
-
                         <p>O manda mensaje de WhatsApp y envía los documentos solicitados.</p>
+                    </div>
+                </div>
+                <div className='arrow'>
+                    <div className='arrow-container'>
+                        <ArrowNextS />
                     </div>
                 </div>
                 <div className="card featured">
@@ -59,28 +69,9 @@ function Requisitos() {
                         </div>
                     </div>
                     <div className="bottom">
-
                         <p>Estás por terminar y a punto de ser tu propio jefe.</p>
                     </div>
                 </div>
-                {/* <div className="card featured">
-                    <div className="top">
-                        <div className='top-img-container'>
-                            <img
-                                className="requisitosImagen"
-                                src="/assets/pasos/paso4.1.png"
-                                alt=""
-                            />
-                        </div>
-                        <div className='top-txt-container'>
-                            <h3>¡Listo! Empieza a maximizar tus ganancias</h3>
-                        </div>
-                    </div>
-                    <div className="bottom">
-
-                        <p>Ya puedes cumplir tus metas.</p>
-                    </div>
-                </div> */}
             </div>
         </div>
     )
