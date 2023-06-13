d2ff3adb7b75dd4c01909856323df29a0ff4aa52
leogaytan18194 <leogaytan02@gmail.com>
2023-06-13 17:17:17 -0600

    actualización sitio web responsivo

--- src/components/segmentos/Segmentos.jsx
+++ src/components/segmentos/Segmentos.jsx
@@ -1,85 +1,85 @@
-import "./segmentos.scss";
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-
-export default function Segmentos() {
+import React from 'react'
+import './segmentos.scss';
 
 
-  return (
-    <div className="segmentos" id="segmentos" style={{
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      height: "100%"
-    }}>
-      <h1 className="ITBold">Segmentos</h1>
-      <div className="container">
-          <Card sx={{ maxWidth: 220, boxShadow: 0 }}>
-            <CardMedia
-              sx={{ height: 220 }}
-              image="assets/segmento1.png"
-              title="green iguana"
-            />
-            <CardContent>
-              <h3 className="ITBold"> 
-                Vinos y Licores
-              </h3>
-              <Typography variant="body2" color="text.secondary">
-              Vestimos vinos y licores que llevan  una historia detrás y forman parte de momentos únicos.
-              </Typography>
-            </CardContent>
-          </Card>
-          <Card sx={{ maxWidth: 230,boxShadow: 0 }}>
-            <CardMedia
-              sx={{ height: 220 }}
-              image="assets/segmento2.png"
-              title="green iguana"
-            />
-            <CardContent>
-              <h3 className="ITBold">
-                Alimentos y Bebidas
-                </h3>
-              <Typography variant="body2" color="text.secondary">
-              Entendemos los retos de la industria de etiquetas para alimentos y te asesoramos con las soluciones más atractivas.
-              </Typography>
-            </CardContent>
-          </Card>
-          <Card sx={{ maxWidth: 220, boxShadow: 0 }}>
-            <CardMedia
-              sx={{ height: 220 }}
-              image="assets/segmento3.png"
-              title="green iguana"
-            />
-            <CardContent>
-              <h3 className="ITBold">
-                Salud y Belleza
-                </h3>
-              <Typography variant="body2" color="text.secondary">
-              La industria cosmética y farmacéutica es  altamente competitiva  el mantenerse a la vanguardia es la clave del éxito para estos productos.
-              </Typography>
-            </CardContent>
-          </Card>
-          <Card sx={{ maxWidth: 220, boxShadow: 0 }}>
-            <CardMedia
-              sx={{ height: 220 }}
-              image="assets/segmento4.png"
-              title="green iguana"
-            />
-            <CardContent>
-              <h3 className="ITBold">
-                Industrial
-                </h3>
-              <Typography variant="body2" color="text.secondary">
-              Conocemos las diversas condiciones a las que se enfrentan los productos de uso industrial.
-              </Typography>
-            </CardContent>
-          </Card>
-          
-      </div>
-      
-    </div>
-  );
+function Segmentos() {
+    return (
+        <div className="segmentos" id="">
+            <div id="comunidad" className='hashlink-id'></div>
+            <h1 className="ITBold">Segmentos</h1>
+            <div className="container">
+                <div className="card featured">
+                    <div className="top">
+                        <div className='top-img-container'>
+                            <img
+                                className="requisitosImagen"
+                                src="/assets/pasos/paso1.png"
+                                alt=""
+                            />
+                        </div>
+                        <div className='top-txt-container'>
+                            <h3>Vinos y Licores</h3>
+                        </div>
+                    </div>
+                    <div className="bottom">
+                        <p>Vestimos vinos y licores que llevan  una historia detrás y forman parte de momentos únicos.</p>
+                    </div>
+                </div>
+                <div className="card featured">
+                    <div className="top">
+                        <div className='top-img-container'>
+                            <img
+                                className="requisitosImagen"
+                                src="/assets/pasos/paso2.png"
+                                alt=""
+                            />
+                        </div>
+                        <div className='top-txt-container'>
+                            <h3>Alimentos y Bebidas</h3>
+                        </div>
+
+                    </div>
+                    <div className="bottom">
+                        <p>Entendemos los retos de la industria de etiquetas para alimentos y te asesoramos con las soluciones más atractivas.</p>
+                    </div>
+                </div>
+                <div className="card featured">
+                    <div className="top">
+                        <div className='top-img-container'>
+                            <img
+                                className="requisitosImagen"
+                                src="/assets/pasos/paso3.png"
+                                alt=""
+                            />
+                        </div>
+                        <div className='top-txt-container'>
+                            <h3>Salud y Belleza</h3>
+                        </div>
+                    </div>
+                    <div className="bottom">
+                        <p>La industria cosmética y farmacéutica es  altamente competitiva  el mantenerse a la vanguardia es la clave del éxito para estos productos.</p>
+                    </div>
+                </div>
+                <div className="card featured">
+                    <div className="top">
+                        <div className='top-img-container'>
+                            <img
+                                className="requisitosImagen"
+                                src="/assets/pasos/paso3.png"
+                                alt=""
+                            />
+                        </div>
+                        <div className='top-txt-container'>
+                            <h3 className="ITBold">Industrial</h3>
+                        </div>
+                    </div>
+                    <div className="bottom">
+                        <p >Conocemos las diversas condiciones a las que se enfrentan los productos de uso industrial.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
+
+export default Segmentos
