ecdc4188ae933af526a189e109bef621d2dfc1b3
leogaytan18194 <leogaytan02@gmail.com>
2023-06-14 09:13:39 -0600

    actualizacion background intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,9 +4,7 @@ export default function Intro() {
 
 
   return (
-    <div className="intro" id="intro" style={{ 
-      height: '450px'
- }}>
+    <div className="intro" id="intro">
         <div className="wrapper">
           <h2 className="helvetica">Las etiquetas de nuestras marcas representan sus valores...</h2>
           <h2 className="helvetica">su personalidad.</h2>
