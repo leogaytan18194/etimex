d5b91d99ed5d316921c49a63c4e6a44be79262e9
leogaytan18194 <leogaytan02@gmail.com>
2023-06-14 09:40:08 -0600

    actualizacion

--- src/components/informacion/Impresiones.jsx
+++ src/components/informacion/Impresiones.jsx
@@ -1,11 +1,10 @@
 import "./impresiones.scss";
-import Background from "./equipo-impresora.jpg";
 
 export default function Impresiones() {
 
 
   return (
-    <div className="impresiones" id="impresiones" style={{ backgroundImage:`url(${Background})`, backgroundRepeat: 'no-repeat',}}>
+    <div className="impresiones" id="impresiones" >
       
         <div className="wrapper">
           <h2 className="helvetica"><b>Nuestra tecnología no tiene límite</b>, desde tiradas cortas, grandes y la flexibilidad para el desarrollo de diferentes empaques.</h2>
