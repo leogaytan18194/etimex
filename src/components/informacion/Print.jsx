ecec39113fa24a38114cb5ac2e9a43bd4e40bd20
leogaytan18194 <leogaytan02@gmail.com>
2023-06-14 10:14:45 -0600

    actualizacion de backgrounds

--- src/components/informacion/Print.jsx
+++ src/components/informacion/Print.jsx
@@ -7,7 +7,7 @@ export default function Print() {
     <div className="print" id="print">
         <div className="wrapper">
           <h1 className="ITBold">Print</h1>
-          <h2>to connect.</h2>
+          <h2>to connect</h2>
         <h3 className="helvetica">Diferenciarnos en el punto de venta es lo mas importante para capta la atención, nuestra gama de materiales, acabaos en formato digital es el futuro.</h3>
         </div>
       </div>
