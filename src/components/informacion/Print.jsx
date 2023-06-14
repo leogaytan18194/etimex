ea25589501cf75c95ea9b0025f3a3c41434a2d70
leogaytan18194 <leogaytan02@gmail.com>
2023-06-14 08:52:08 -0600

    actualizacion

--- src/components/informacion/Print.jsx
+++ src/components/informacion/Print.jsx
@@ -8,7 +8,7 @@ export default function Print() {
     <div className="print" id="print" style={{ backgroundImage:`url(${Background})`, backgroundRepeat: 'no-repeat',}}>
         <div className="wrapper">
           <h1 className="ITBold">Print</h1>
-          <h2>to connect</h2>
+          <h2>to connect.</h2>
         <h3 className="helvetica">Diferenciarnos en el punto de venta es lo mas importante para capta la atención, nuestra gama de materiales, acabaos en formato digital es el futuro.</h3>
         </div>
       </div>
