b93b57b83860830dd55380e115ae0158c8e42af6
leogaytan18194 <leogaytan02@gmail.com>
2023-06-24 20:47:40 -0600

    actualización de ruta

--- src/routes/PageRoutes.jsx
+++ src/routes/PageRoutes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/home/Home'
 import Vinos from '../pages/segmentos/Vinos'
+import SaludBelleza from '../pages/segmentos/SaludBelleza'
 import AlimentosBebidas from '../pages/segmentos/AlimentosBebidas'
 import Tintas from '../pages/secciones/Tintas'
 
@@ -12,6 +13,7 @@ const PageRoutes = () => {
             <Route path="/vinos-y-licores" element={<Vinos />} />
             <Route path="/alimentos-y-bebidas" element={<AlimentosBebidas />} />
             <Route path="/servicios/tintas" element={<Tintas />} />
+            <Route path="/salud-y-belleza" element={<SaludBelleza />} />
 
         </Routes>
     )
