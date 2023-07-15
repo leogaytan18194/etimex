1b121c51afb91230aae5c613818201dfdb51fbec
leogaytan18194 <49291846+leogaytan18194@users.noreply.github.com>
2023-07-15 15:31:42 -0600

    Update Clientes.jsx
    Actualizacion de acento

--- src/components/clientes/Clientes.jsx
+++ src/components/clientes/Clientes.jsx
@@ -57,7 +57,7 @@ export default function Clientes() {
             <img src="assets/clientes.png" alt="Clientes Etimex" className="imgClientes"/>
           </div>
           <div className='txt-more-clients'>
-              <h3>y mas...</h3>
+              <h3>y más...</h3>
           </div>
       </div>
       
