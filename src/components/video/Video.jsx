30bd9bcd073a097abce20f7873773cebc76cbe50
leogaytan18194 <leogaytan02@gmail.com>
2023-07-16 21:34:37 -0600

    video etimex

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -5,7 +5,7 @@ export default function Video() {
 
   return (
 
-      <video autoPlay loop muted id="video">
+      <video autoPlay loop muted playsinline id="video">
         <source src="./assets/video-oficial.mp4" type="video/mp4" />
       </video>
 
