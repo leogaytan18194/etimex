2c6a31b02b89f34c7e48d7fb0962c581c74bc484
leogaytan18194 <leogaytan02@gmail.com>
2023-07-16 19:50:53 -0600

    video mp4

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -1,11 +1,10 @@
 import "./video.scss";
 
-
 export default function Video() {
 
 
   return (
-    <video loop muted autoplay playsinline id="video">
+    <video autoPlay loop muted id="video">
       <source src="./assets/video-oficial.mp4" type="video/mp4" />
     </video>
   );
