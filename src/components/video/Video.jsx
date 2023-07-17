c740a00cc77aa6e5aec2a3f827fb985767e0e6cb
leogaytan18194 <leogaytan02@gmail.com>
2023-07-16 20:07:02 -0600

    video en mobile

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -4,8 +4,10 @@ export default function Video() {
 
 
   return (
-    <video autoPlay loop muted id="video">
-      <source src="./assets/video-oficial.mp4" type="video/mp4" />
-    </video>
+    <div className="fullscreen-video-wrap" dangerouslySetInnerHTML={{ __html: `
+      <video autoPlay loop muted id="video">
+        <source src="./assets/video-oficial.mp4" type="video/mp4" />
+      </video>
+    ` }}></div>
   );
 }
