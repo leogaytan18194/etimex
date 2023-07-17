15224f5e38b8c60ed9d4877b100f55ee2bcf3722
leogaytan18194 <leogaytan02@gmail.com>
2023-07-16 19:21:14 -0600

    actualizacion de video

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -6,6 +6,7 @@ export default function Video() {
   return (
     <video autoPlay loop muted id="video">
       <source src="./assets/web-oficial_2.webm" type="video/webm" />
+      <source src="./assets/video-etimex.mp4" type="video/mp4" />
     </video>
   );
 }
