db529eaa78e8050909c441142dfc6623a5c4a920
leogaytan18194 <leogaytan02@gmail.com>
2023-07-16 20:22:27 -0600

    video etimex

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -4,10 +4,10 @@ export default function Video() {
 
 
   return (
-    <div className="fullscreen-video-wrap" dangerouslySetInnerHTML={{ __html: `
+
       <video autoPlay loop muted id="video">
         <source src="./assets/video-oficial.mp4" type="video/mp4" />
       </video>
-    ` }}></div>
+
   );
 }
